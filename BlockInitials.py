39ea9e49d2f5a48aa8de02cd21f27d2abb334d4c
poni35 <96385870+poni35@users.noreply.github.com>
2024-12-08 21:17:29 +0000

    Add files via upload

---
+++ BlockInitials.py
@@ -0,0 +1,9 @@
+#Paul O#
+#I run a lot#
+print("PPPP    OOO")
+print("P   P  O   O")  
+print("P   P  O   O")  
+print("PPPP   O   O")  
+print("P      O   O")  
+print("P      O   O")  
+print("P       OOO")  
